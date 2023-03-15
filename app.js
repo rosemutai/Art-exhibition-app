4182972f5c9b76247ff6703f06fe66596b32dff9
rosemutai <chepngetichrose2030@gmail.com>
2023-03-15 22:33:48 +0300

    Modify projects' section

--- app.js
+++ app.js
@@ -42,19 +42,54 @@ const speakers = [
   },
 ];
 
+const closeNavIcon = document.getElementById('close-icon');
+const nav = document.getElementById('navbarNavAltMarkup');
+
+closeNavIcon.addEventListener('click', () => {
+  nav.style.display = 'none';
+});
+
 const speakersSection = document.getElementById('speakers-section');
-speakers.slice(0, 2).forEach((speaker) => {
+const desktopSpeakersSection = document.getElementById('desktop-speakers-section');
+
+// desktop version speakers
+speakers.forEach((speaker) => {
   const card = document.createElement('div');
-  // card.classList.add('col-md-4', 'mb-4');
-  speakersSection.style.height = '50vh';
 
   const cardContent = `
-    <div class='speaker-card card px-2  bg-light d-flex flex-row mb-3
+    <div class='speaker-card card px-2 d-flex flex-row mb-3
       justify-content-center align-items-center rounded-0 border-0'>
-      <img src=${speaker.image} class="cardi" style="width: 100px; height: 120px;" alt="speaker">
+      <img src=${speaker.image} class="speaker-image" style="background-image: 
+        'images/chess.png'; width: 100px; height: 120px;" alt="speaker">
+      <img src='images/chess.png' class='chess position-absolute' alt='chess'>
       <div class='speaker-details ms-4'>
         <h5 class='cardtitle fw-bold text-dark'>${speaker.name}</h5>
         <p class='cardtitle fw-medium opacity-100 fst-italic'>${speaker.careerRole}</p>
+        <hr class='my-1 ms-0 speaker-hr' >
+        <p class='cartex fw-light text-opacity-25 small'>${speaker.details}</p>
+      </div>
+    </div>
+  `;
+
+  card.innerHTML = cardContent;
+  desktopSpeakersSection.appendChild(card);
+});
+
+// mobile version speakers
+speakers.slice(0, 2).forEach((speaker) => {
+  const card = document.createElement('div');
+  // card.classList.add('col-md-4', 'mb-4');
+
+  const cardContent = `
+    <div class='card mobile-speaker-card px-3 d-flex flex-row mb-3 gap-2
+      justify-content-center align-items-center rounded-0 border-0'>
+      <img src=${speaker.image} class="speaker-image" style="background-image: 'images/chess.png'; 
+        width: 100px; height: 120px;" alt="speaker">
+      <img src='images/chessblack.png' class='black-chess position-absolute' alt='chess'>
+      <div class='speaker-details ms-4'>
+        <h5 class='cardtitle fw-bold text-dark'>${speaker.name}</h5>
+        <p class='cardtitle fw-medium opacity-100 fst-italic'>${speaker.careerRole}</p>
+        <hr class='my-1 ms-0 speaker-hr' >
         <p class='cartex fw-light text-opacity-25 small'>${speaker.details}</p>
       </div>
     </div>
@@ -69,12 +104,13 @@ const shoreMoreBtn = document.getElementById('show-more-btn');
 shoreMoreBtn.addEventListener('click', () => {
   speakers.slice(2).forEach((speaker) => {
     const card = document.createElement('div');
-    speakersSection.style.height = '100vh';
+    speakersSection.style.marginTop = '2rem';
 
     const cardContent = `
-    <div class='speaker-card card px-2 bg-light d-flex flex-row mb-3
+    <div class=' mobile-speaker-card px-2 bg-light d-flex flex-row mb-3
       justify-content-center align-items-center rounded-0 border-0'>
-      <img src=${speaker.image} class="cardi" style="width: 100px; height: 120px;" alt="speaker">
+      <img src=${speaker.image} class="speaker-image" style="width: 100px; height: 120px;" alt="speaker">
+      <img src='images/chessblack.png' class='black-chess position-absolute' alt='chess'>
       <div class='speaker-details ms-4'>
         <h5 class='cardtitle fw-bold text-dark'>${speaker.name}</h5>
         <p class='cardtitle fw-medium opacity-100 fst-italic'>${speaker.careerRole}</p>
